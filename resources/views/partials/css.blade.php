85ebeb3cd8097dde761a7f5914ec8954c76d11ba
AbdulmalikAbdulwakeel <abdulmalikabdulwakeel7@gmail.com>
2025-08-27 13:14:38 +0100

    home page

---
+++ resources/views/partials/css.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="description" content="Your Ultimate Job HTML Template">
+<meta name="keywords" content="Hosting, Domain, Transfer, Buy Domain, WHMCS">
+<link rel="canonical" href="https://html.themewant.com/hostie">
+<meta name="robots" content="index, follow">
+
+<!-- Open Graph -->
+<meta property="og:title" content="Your Ultimate Job HTML Template">
+<meta property="og:description" content="Your Ultimate Job HTML Template">
+<meta property="og:image" content="https://www.example.com/image.jpg">
+<meta property="og:url" content="https://html.themewant.com/hostie/">
+
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Your Ultimate Job HTML Template">
+<meta name="twitter:description" content="Your Ultimate Job HTML Template">
+<meta name="twitter:image" content="{{ asset('landing/assets/images/banner/slider-img-01.webp') }}">
+
+<!-- Favicon -->
+<link rel="icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('assets/img/favicon-16x16.png') }}" type="image/x-icon">
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap" rel="stylesheet">
+
+<!-- CSS Files -->
+<link rel="stylesheet" href="{{ asset('assets/fonts/icon/css/rt-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome/fontawesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/plugins.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <title>{{ $page_title ?? 'Best Work - International Manpower & Resources, Inc.' }}</title>
+</head>
